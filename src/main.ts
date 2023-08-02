186c4185b999f8d88ca1bf573ef2878c4e777620
Vernon Kesner <vernonkesner@gmail.com>
2023-08-02 17:29:49 -0400

    render layout

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,44 @@
+import type { Story } from './stories';
+
+import menu from './menu';
+import visualMode from './visual-mode';
+import stories from './stories';
+import comments from './comments';
+import user from'./user';
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Hacker News Clone</h1>
-  </div>
-`;
+async function render() {
+  const appRoot = document.querySelector<HTMLDivElement>('#app')!;
+  const layoutContainer = document.createDocumentFragment();
+  const header = document.createElement('header');
+  header.appendChild(menu());
+  header.appendChild(visualMode());
+  layoutContainer.appendChild(header);
+  const main = document.createElement('main');
+  const storiesEl = await stories('top');
+  main.appendChild(storiesEl);
+  layoutContainer.appendChild(main);
+  appRoot.appendChild(layoutContainer);
+
+  document.addEventListener('story:comments', async (event: Event) => {
+    const customEvent = event as CustomEvent<{ id: number, story: Story }>;
+    const { detail } = customEvent;
+    const { story } = detail;
+    // append comments container to main
+    const commentsContainer = await comments(story);
+    main.innerHTML = '';
+    main.appendChild(commentsContainer);
+  });
+
+  document.addEventListener('story:user', async (event: Event) => {
+    const customEvent = event as CustomEvent<{ id: string }>;
+    const { detail } = customEvent;
+    const { id } = detail;
+    console.log('user', id);
+    const userContainer = await user(id);
+    main.innerHTML = '';
+    main.appendChild(userContainer);
+  });
+}
+
+render();
